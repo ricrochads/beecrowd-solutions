eedacb3cafdd4144eb8a278427a9f69f9a3a9fde
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-02-05 22:44:13 -0300

    Area of a Circle

--- 01.Beginner/1002_AreaOfACircle.cs
+++ 01.Beginner/1002_AreaOfACircle.cs
@@ -1,9 +1,9 @@
 using System; 
 
-class URI {
-
-    static void Main(string[] args) { 
-        
+class URI 
+{
+    static void Main(string[] args) 
+    {     
         double R, A, pi = 3.14159;
 
         R = double.Parse(Console.ReadLine());
@@ -11,7 +11,5 @@ class URI {
         A = pi * R * R;
 
         Console.WriteLine("A=" + A.ToString("F4"));
-        
     }
-    
 }
