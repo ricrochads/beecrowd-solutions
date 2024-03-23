e262a9f8ebef30d56f7245edefe98e28f5281646
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-03-23 00:46:45 -0300

    1047 - Game Time with Minutes

---
+++ 01.Beginner/1047 - Game Time with Minutes/1047_GameTimeWithMinutes.cs
@@ -0,0 +1,38 @@
+using System;
+
+class URI 
+{
+    static void Main(string[] args) 
+    {
+        int initialHour, initialMinute, finalHour, finalMinute, durationHour, durationMinute;
+
+        string[] gameTime = Console.ReadLine().Split(' ');
+
+        initialHour = int.Parse(gameTime[0]);
+        initialMinute = int.Parse(gameTime[1]);
+        finalHour = int.Parse(gameTime[2]);
+        finalMinute = int.Parse(gameTime[3]);
+
+        int initialTimeMinutes = initialHour * 60 + initialMinute;
+        int finalTimeMinutes = finalHour * 60 + finalMinute;
+
+        int duration = finalTimeMinutes - initialTimeMinutes;
+        
+        if (initialHour == finalHour && initialMinute == finalMinute)
+        {
+            Console.WriteLine($"O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)");
+        }
+        else if (duration > 0)
+        {
+            durationHour = duration / 60;
+            durationMinute = duration % 60;
+            Console.WriteLine($"O JOGO DUROU {durationHour} HORA(S) E {durationMinute} MINUTO(S)");
+        }
+        else
+        {
+            durationHour = 23 + (duration / 60);
+            durationMinute = 60 + (duration % 60);
+            Console.WriteLine($"O JOGO DUROU {durationHour} HORA(S) E {durationMinute} MINUTO(S)");
+        }
+    }
+}
