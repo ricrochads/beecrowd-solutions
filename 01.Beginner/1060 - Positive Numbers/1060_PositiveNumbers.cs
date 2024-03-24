ea57eff25a3bd96b79c74629699b768f96c20400
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-03-24 18:15:35 -0300

    1060 - Positive Numbers

---
+++ 01.Beginner/1060 - Positive Numbers/1060_PositiveNumbers.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+class URI 
+{
+    static void Main(string[] args) 
+    {
+        double[] numbers = new double[6];
+
+        for (int i = 0; i < 6; i++)
+        {
+            numbers[i] = double.Parse(Console.ReadLine());
+        }
+        
+        List<double> positiveNumbers = new List<double>();
+
+        foreach (double number in numbers)
+        {
+            if (number > 0)
+            {
+                positiveNumbers.Add(number);
+            }
+        }
+
+        Console.WriteLine($"{positiveNumbers.Count} valores positivos");
+    }
+}
