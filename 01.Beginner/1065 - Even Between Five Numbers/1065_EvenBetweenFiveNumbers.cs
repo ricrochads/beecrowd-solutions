7e014128a9e619aecf91d846fe7a4f77c1134c77
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-03-24 18:25:11 -0300

    1065 - Even Between Five Numbers

---
+++ 01.Beginner/1065 - Even Between Five Numbers/1065_EvenBetweenFiveNumbers.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+class URI 
+{
+    static void Main(string[] args) 
+    {
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+        
+        List<int> evenNumbers = new List<int>();
+
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                evenNumbers.Add(number);
+            }
+        }
+
+        Console.WriteLine($"{evenNumbers.Count} valores pares");
+    }
+}
--- 01.Beginner/1065 - Even Between Five Numbers/README.md
+++ 01.Beginner/1065 - Even Between Five Numbers/README.md
@@ -1,4 +1,4 @@
-# 1065 - Even Between five Numbers
+# 1065 - Even Between Five Numbers
 
 Make a program that reads five integer values. Count how many of these values ​​are even and  print this information like the following example.
 
