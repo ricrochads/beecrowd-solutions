840c53beeae13b5d961e8d2d311c8a5923a2ef3f
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-03-25 23:57:45 -0300

    1096 - Sequence IJ 2

---
+++ 01.Beginner/1096 - Sequence IJ 2/1096_SequenceIJ2.cs
@@ -0,0 +1,19 @@
+using System;
+
+class URI 
+{
+    static void Main(string[] args) 
+    {
+        int i = 1;
+        int j = 7;
+
+        do
+        {
+            for (int index = 0; index < 3; index++)
+            {
+                Console.WriteLine($"I={i} J={j - index}");
+            }
+            i += 2;
+        } while (i <= 9);
+    }
+}
