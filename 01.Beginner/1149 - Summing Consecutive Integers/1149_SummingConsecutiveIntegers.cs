f1b77eae2919b40a269a6b690f09ac67797bcdff
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-03-30 13:33:28 -0300

    1149 - Summing Consecutive Integers

---
+++ 01.Beginner/1149 - Summing Consecutive Integers/1149_SummingConsecutiveIntegers.cs
@@ -0,0 +1,31 @@
+using System;
+
+class URI 
+{
+    static void Main(string[] args) 
+    {
+        string[] values = Console.ReadLine().Split(' ');
+
+        int a = int.Parse(values[0]);
+
+        for (int i = 1; i <= values.Length; i++)
+        {
+            if (int.Parse(values[i]) > 0)
+            {
+                values[1] = values[i];
+                break;
+            }
+        }
+
+        int n = int.Parse(values[1]);
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += a + i;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
