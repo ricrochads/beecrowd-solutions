13d00373148a1eb35d9509eb4479264117a6575b
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-03-30 22:03:30 -0300

    1172 - Array Replacement I

---
+++ 01.Beginner/1172 - Array Replacement I/1172_ArrayReplacementI.cs
@@ -0,0 +1,19 @@
+using System;
+
+class URI 
+{
+    static void Main(string[] args) 
+    {
+        int[] x = new int[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            x[i] = int.Parse(Console.ReadLine());
+
+            if (x[i] <= 0)
+                x[i] = 1;
+            
+            Console.WriteLine($"X[{i}] = {x[i]}");
+        }
+    }
+}
