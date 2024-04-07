2701e2ec6dfbead0a6fe4bad84603713aab3c18c
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-04-06 21:27:32 -0300

    1930 - Electrical Outlet

--- 01.Beginner/1930 - Electrical Outlet/1930_ElectricalOutlet.cs
+++ 01.Beginner/1930 - Electrical Outlet/1930_ElectricalOutlet.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Linq;
 
-internal class Program
+class URI 
 {
-    private static void Main(string[] args)
+    static void Main(string[] args) 
     {
         var line = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
         var (t1, t2, t3, t4) = (line[0], line[1], line[2], line[3]);
