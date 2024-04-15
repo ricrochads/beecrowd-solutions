ed3911de210f439a9d530a0a647bec1eb31b3251
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-04-14 23:23:53 -0300

    2581 - I am Toorg!

---
+++ 01.Beginner/2581 - I am Toorg!/2581_IAmToorg.cs
@@ -0,0 +1,15 @@
+using System;
+
+class URI 
+{
+    static void Main(string[] args) 
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string question = Console.ReadLine();
+            Console.WriteLine("I am Toorg!");
+        }
+    }
+}
