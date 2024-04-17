12f1c9afefff9142d8ea2f173a952f086c3c4003
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-04-16 23:28:02 -0300

    2747 - Output 1

---
+++ 01.Beginner/2747 - Output 1/2747_Output1.cs
@@ -0,0 +1,14 @@
+using System;
+
+class URI 
+{
+    static void Main(string[] args) 
+    {
+        Console.WriteLine(new string('-', 39));
+
+        for (int i = 1; i <= 5; i++)
+            Console.WriteLine("|" + new string(' ', 37) + "|");
+
+        Console.WriteLine(new string('-', 39));
+    }
+}
