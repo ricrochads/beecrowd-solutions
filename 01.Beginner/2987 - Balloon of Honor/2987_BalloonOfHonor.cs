5c1ed1a61d37d20aaead9eba25ff939c77d5167b
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-04-23 23:34:18 -0300

    2987 - Balloon of Honor

--- 01.Beginner/2987 - Balloon of Honor/2987_BalloonOfHonor.cs
+++ 01.Beginner/2987 - Balloon of Honor/2987_BalloonOfHonor.cs
@@ -5,8 +5,10 @@ class URI
     static void Main(string[] args) 
     {
         char l = Console.ReadLine()[0];
+
         l = char.ToUpper(l);
         int position = l - 'A' + 1;
+        
         Console.WriteLine(position);        
     }
 }
