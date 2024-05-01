e4982da6404788a8214a10b0dbcaa12a363b196f
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-04-30 23:17:30 -0300

    1003 - Simple Sum

---
+++ 01. Beginner/1003 - Simple Sum/1003_SimpleSum.cs
@@ -0,0 +1,16 @@
+using System; 
+
+class URI 
+{
+    static void Main(string[] args) 
+    {
+        int a, b, soma;
+        
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+
+        soma = a + b;
+
+        Console.WriteLine("SOMA = " + soma);
+    }
+}
