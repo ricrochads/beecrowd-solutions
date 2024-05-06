93fb37fb5658b523bfbe83b88c5fc1bc7d6bf1a2
Ricardo Rocha <139723976+ricrochads@users.noreply.github.com>
2024-05-06 00:10:22 -0300

    1075 - Remaining 2

---
+++ 01. Beginner/1075 - Remaining 2/1075_Remaining2.cs
@@ -0,0 +1,15 @@
+using System;
+
+class URI 
+{
+    static void Main(string[] args) 
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 10000; i++)
+        {
+            if (i % n == 2)
+                Console.WriteLine(i);
+        }
+    }
+}
